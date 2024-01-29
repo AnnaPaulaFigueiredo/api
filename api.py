c52a97a4ce8f2aa3d1fce429aafde84c8efc713f
Anna Paula <annapaulafigueiredoo@gmail.com>
2024-01-29 09:43:42 -0300

    add filtered by year

--- api.py
+++ api.py
@@ -1,8 +1,17 @@
 from flask import Flask, jsonify, request
 from google_play_scraper import reviews_all, Sort
 
-app = Flask(__name__)
+# FUNCITONS
+def filter_by_year(json_list, years):
+    filtered_list = []
+    for item in json_list:
+        at_year = item.get("at").year
+        if at_year in years:
+            filtered_list.append(item)
+    return filtered_list
 
+# ROUTES
+app = Flask(__name__)
 @app.route('/api-playstore')
 def homepage():
     return "A api está no ar."
@@ -17,6 +26,7 @@ def get_data():
         return jsonify({'error': 'Parâmetro "link" ausente na URL'}), 400
     
     app_reviews = reviews_all(link, lang='pt', country='br', sort=Sort.NEWEST)
+    app_reviews = filter_by_year(app_reviews, [2024, 2023] )
     reviews_json = [{"content": item["content"], "replyContent": item["replyContent"]} for item in app_reviews]
  
     return jsonify(reviews_json)
